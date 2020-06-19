ae83ec6d79df3a9e1775ce601fd4a6972e9c55f8
mssivakumar02 <66684898+mssivakumar02@users.noreply.github.com>
2020-06-19 11:15:15 -0400

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	    System.out.println("This is for demo");
+	    System.out.println("This is for demo - 2");
 	}
 
 }
